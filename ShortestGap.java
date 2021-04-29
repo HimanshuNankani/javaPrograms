7ed484826c6a7416c8d20e57d329b7a4cb725375
Himanshu Nankani <52562516+HimanshuNankani@users.noreply.github.com>
2021-04-29 17:36:18 +0530

    user input

--- ShortestGap.java
+++ ShortestGap.java
@@ -1,41 +1,56 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class ShortestGap { 
   
   public static int shortestFinder(String doc[],String first, String second){
   
     ArrayList<Integer> xList = new ArrayList<Integer>();
-    
+
     for(int i=0; i<doc.length;i++){
-    	if(doc[i]==first){
+    	if(doc[i].equals(first)){
         	for(int j=i+1; j>i && j<doc.length;j++){
-            	if(doc[j]==second){
+            	if(doc[j].equals(second)){
                 	xList.add(j-i-1);
                 }
             }
         }
-      	if(doc[i]==second){
+      	if(doc[i].equals(second)){
         	for(int k=i+1; k>i && k<doc.length;k++){
-            	if(doc[k]==first){
+            	if(doc[k].equals(first)){
                 	xList.add(k-i-1);
                 }
             }
         }
     }
-    
+
     Collections.sort(xList);
-    return xList.get(0);
+    return xList.size()>0 ? xList.get(0) : -1;
   
   }
   
   public static void main(String[] args) {
-
-    String[] input_array= {"ab","bc","cd","de","ef","ab","def","ghi"};
-
-    String fromString = "ab";
-    String toString = "de";
-    System.out.println(shortestFinder(input_array, toString, fromString)); 
+    Scanner myobj = new Scanner(System.in);
+    
+    System.out.print("Enter no.of elements : ");
+    int n = myobj.nextInt();
+    myobj.nextLine();
+    
+    String[] input_array= new String[n];
+    
+    System.out.println("Enter the elements : ");
+    for(int x=0; x<n; x++){
+        input_array[x] = myobj.nextLine();
+    }
+    
+    System.out.print("FromString : ");
+    String fromString = myobj.nextLine();
+    
+    System.out.print("ToString : ");
+    String toString = myobj.nextLine();
+    
+    System.out.println(shortestFinder(input_array, fromString, toString)); 
   } 
   
 }
